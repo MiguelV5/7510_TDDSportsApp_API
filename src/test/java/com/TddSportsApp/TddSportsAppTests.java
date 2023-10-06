349a55205fae06b1d3e208564d1812f104161451
Rocío <rplatini@fi.uba.ar>
2023-10-05 21:38:09 -0300

    Create docker compose

--- src/test/java/com/TddSportsApp/TddSportsAppTests.java
+++ src/test/java/com/TddSportsApp/TddSportsAppTests.java
@@ -1,13 +1,17 @@
 package com.TddSportsApp;
 
-// import org.junit.jupiter.api.Test;
-// import org.springframework.boot.test.context.SpringBootTest;
+ import org.junit.jupiter.api.Test;
+ import org.springframework.boot.test.context.SpringBootTest;
 
-// @SpringBootTest
-// class TddSportsAppTests {
+ @SpringBootTest
+ class TddSportsAppTests {
 
-// @Test
-// void contextLoads() {
-// }
+     @Test
+     void contextLoads() {
+     }
 
-// }
+     @Test
+     void testInitial() {
+         assert true;
+     }
+ }
